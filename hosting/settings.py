c735ef179dcf8673e8522f28fdd999031a19c45d
prasanth_M.V <93965482+Prasan1234@users.noreply.github.com>
2023-08-30 18:34:44 +0530

    Update settings.py

--- hosting/settings.py
+++ hosting/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-x=jxlx^l3=s9&#-ub65q5xmiz^0lwsqkesw1jsrbcqqlc%fxnn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hosting_app',
 ]
 
 MIDDLEWARE = [
